b01a560ac539edec60e305c7adac6969c978b433
puncharrow5 <osh4761@hanmail.net>
2025-05-06 00:03:05 +0900

    feat: 이메일 발송 API

--- src/email/email.resolver.ts
+++ src/email/email.resolver.ts
@@ -1,7 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { EmailService } from './email.service';
+import { SendEmailArgs } from './dto';
 
 @Resolver()
 export class EmailResolver {
   constructor(private readonly emailService: EmailService) {}
+
+  @Mutation(() => Boolean, { description: '이메일 발송' })
+  async SendEmail(@Args() sendEmailArgs: SendEmailArgs) {
+    return await this.emailService.sendEmail(sendEmailArgs);
+  }
 }
